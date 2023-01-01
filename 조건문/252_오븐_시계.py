b84a93f7c431dcacb857371f5117e42526133b0d
yerim <107531558+Hoyeri@users.noreply.github.com>
2023-01-01 21:30:44 +0900

    Create 252_오븐_시계.py

---
+++ 조건문/252_오븐_시계.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split()) #current time, h, m
+c = int(input()) #cooking time, m
+
+if b+c >= 60:
+    a += (b+c) // 60
+    b = (b+c) % 60
+else:
+    b += c
+if a >= 24:
+    a -= 24
+
+print(a, b)
